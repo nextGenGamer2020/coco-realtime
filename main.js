75ae9fb47ddb9904713e5abceba77a9b68dab0bb
nextGenGamer2020 <85717794+nextGenGamer2020@users.noreply.github.com>
2022-04-24 19:02:35 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -19,18 +19,18 @@ function setup(){
 function draw(){
     image(video, 0,0, 350,350)
     if(status1 != ""){
-        R = random(255)
-        G = random(255)
-        B = random(255)
+        r = random(255)
+        g = random(255)
+        b = random(255)
         objectDetector.detect(video, gotResults)
         for(i = 0;i< objects.length;i++){
             document.getElementById("status").innerHTML = "Status : Object Detected";
             document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected Are: "+objects.length;
-            fill(R,G,B)
+            fill(r,g,b)
             percent = floor(objects[i].confidence * 100)
             text(objects[i].label+" "+percent + "%", objects[i].x, objects[i].y)
             nofill()
-            stroke(R,G,B)
+            stroke(r,g,b)
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
     
