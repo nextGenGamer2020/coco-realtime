b425eb6ccca81090627ec6b8de743a6f156d4ee2
nextGenGamer2020 <85717794+nextGenGamer2020@users.noreply.github.com>
2022-05-06 17:50:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,61 +1,58 @@
+object = [];
+video = ""
 status1 = ""
-objects = []
-
 function preload(){
-
+    video = createVideo("video.mp4")
+    video.hide()
 }
+
 
 function setup(){
     canvas = createCanvas(350,350)
     canvas.center()
-    video = createCapture(VIDEO)
-    video.size(350,350)
-    video.hide()
-    objectDetector = ml5.objectDetector("cocossd", modelLoaded)
-    document.getElementById("status").innerHTML = "Status : Object Detected";
+
 
 }
+
 
 function draw(){
-    image(video, 0,0, 350,350)
+    image(video, 0, 0, 350,350)
     if(status1 != ""){
-        r = random(255)
-        g = random(255)
-        b = random(255)
         objectDetector.detect(video, gotResults)
-        for(i = 0;i< objects.length;i++){
-            document.getElementById("status").innerHTML = "Status : Object Detected";
-            document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected Are: "+objects.length;
-            fill(r,g,b)
-            percent = floor(objects[i].confidence * 100)
-            text(objects[i].label+" "+percent + "%", objects[i].x, objects[i].y)
-            nofill()
-            stroke(r,g,b)
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        
+        for(i = 0; i<objects.length; i++){
+            document.getElementById("status").innerHTML = "Objects Detected: "
+            document.getElementById("number_of_objects").innerHTML = "Number of object detected are: "+objects.length;
+
+            fill("FF0000")
+            percent = floor(objects[i].confidence*100)
+            text(objects[i].label+  ""+ percent + "%", objects[i].x + 15 +  objects[i].y + 15)
         }
-    
     }
 
+  
 }
 
+function start(){
+    objectDetector = ml5.objectDetector("cocossd", modeLoaded)
+    document.getElementById("status").innerHTML = "Status: detecting objects"
+}
 
-
-
-function modelLoaded(){
+function modeLoaded(){
     console.log("Model Loaded!")
     status1 = true
-    
+    video.loop()
+    video.speed(1)
+    video.volume(0)
 }
+
 
 function gotResults(error, results){
     if(error){
         console.error(error)
-
     }
-        console.log(results)
-        objects = results
-    }
-
-
+    console.log(results)
+    object.results()
+}
 
 
